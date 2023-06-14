7c274a5a354b46e359f7800e577302ce6cc9d3e6
luckyfullstack2023 <luckyfullstack2023@gmail.com>
2023-06-14 10:12:13 +0530

    L-38 assignment done sir

---
+++ js/custom.js
@@ -0,0 +1,28 @@
+
+// syntax
+// var variablename = variablevalue;
+
+
+// create a function
+// 1.Function Defination/Name define
+// Function syntax
+/* 
+   function <functionName(){
+    // Function body
+   }
+*/
+// eg:-
+//  I. Function defination is a one-time process
+
+    function greetMyStudents(name){  // we can pass some actual arguments//camelCase 
+
+    console.log("Hello" + name);  //  concatination
+}
+// II. Invoking the function means calling the function
+//     function calling is a many time process
+
+    greetMyStudents(" sir");  // we can pass some actual arguments
+    greetMyStudents(' L1');
+    greetMyStudents(` Lucky`);
+
+// 2.Function Calling/Invocation Call/Invoke
---
+++ js/custom2.js
@@ -0,0 +1,14 @@
+// make a function that add two number
+
+// I will define the function name with camelCase
+// 1. Function defination is a one time process
+function addTwoNumbers(n1,n2){
+    // function body
+    console.log(n1 - 5);
+    console.log(n2 - 5);
+
+    
+}
+
+// 2. i can calling/Invoking the function as many time as i want
+ addTwoNumbers(10,20);
